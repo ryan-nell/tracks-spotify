3adab029b751c09a5f5067014d1ba0975b5974c2
Ryan Nell <ryannell@Ryans-MacBook-Pro.local>
2020-05-26 21:19:24 -0400

    Map and render individual Tracks

--- src/Components/Tracks.js
+++ src/Components/Tracks.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react'
 
 class Tracks extends  Component {
     render() {
+        const { tracklist } = this.props
         return(
-            <div>This is the Track Component</div>
+            <div className="tracklist-container">
+                <div className="tracklist-div">
+                    {
+                        tracklist.map( track => ( 
+                            <div className="track" key={track.id}>
+                                <img className="track-album" src={track.albumArt} alt="album artwork" />
+                                <p>{track.name}</p>
+                                <p>{track.artist}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         )
     }
 }
