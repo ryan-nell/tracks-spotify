ce0b943ace8cd93ce223bb520f097f7b5a87c7f2
Ryan Nell <ryannell@Ryans-MacBook-Pro.local>
2020-05-30 00:25:45 -0400

    Remove map method

--- src/Components/Tracks.js
+++ src/Components/Tracks.js
@@ -6,42 +6,49 @@ import '../App.css'
 class Tracks extends  Component {
     
     state = {
-        isPlaying: true
+        isPlaying: false,
     }
 
+    // Render icon based on the state of isPlaying
     renderAction() {
-        if(!this.state.playing) {
-            return <FontAwesomeIcon className="play-button" icon={faPlayCircle} />
+        if(!this.state.isPlaying) {
+            return <FontAwesomeIcon className="play-button" icon={faPlayCircle} onClick={this.changeMediaControls}/>
         }
         else {
-            return <FontAwesomeIcon className="pause-button" icon={faPauseCircle} />
+            return <FontAwesomeIcon className="pause-button" icon={faPauseCircle} onClick={this.changeMediaControls}/>
+        }
+    }
+
+    // Change state
+    changeMediaControls = () => {
+        if(this.state.isPlaying) {
+            this.setState({
+                isPlaying: false,     
+            }) 
+        } else {
+            this.setState({
+                isPlaying: true,     
+            })
         }
     }
 
     render() {
-        const { tracklist } = this.props
-        return(
-            <div className="tracklist-container">
-                <div className="tracklist-div">
-                    {
-                        tracklist.map( track => ( 
-                            <div className="track" key={track.id}>
-                                <img className="track-album" src={track.image} alt="album artwork" />
-                                <div className="track-details-container">
-                                    <div className="track-details">
-                                        <div className="track-buttons">
-                                            {this.renderAction()}
-                                            <FontAwesomeIcon className="like-button" icon={faHeart}/>
-                                        </div>
-                                        <p className="details">{track.artist} | {track.name}</p>
-                                        <p className="details">{track.album}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    }
+        const {track} = this.props
+
+        return(                
+            <div className="track" >
+                <img className="track-album" src={track.image} alt="album artwork" />
+                <div className="track-details-container">
+                    <div className="track-details">
+                        <div className="track-buttons">
+                            {this.renderAction()}
+                            <FontAwesomeIcon className="like-button" icon={faHeart}/>
+                        </div>
+                        <p className="details">{track.artist} | {track.name}</p>
+                        <p className="details">{track.album}</p>
+                    </div>
                 </div>
-            </div>
+            </div>           
         )
     }
 }
