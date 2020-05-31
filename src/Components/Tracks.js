9f9f33176ebdc3b0cffb84f667f2d29e99f6771a
Ryan Nell <ryannell@Ryans-MacBook-Pro.local>
2020-05-31 16:56:40 -0400

    add likeSong and toggleAudio method and change conditional statements

--- src/Components/Tracks.js
+++ src/Components/Tracks.js
@@ -5,31 +5,51 @@ import '../App.css'
 
 class Tracks extends  Component {
     
+    // Create new Audio object
+    audio = new Audio(this.props.track.previewTrack); 
+
     state = {
         isPlaying: false,
+        isPaused: true, 
+        isLiked: false
     }
-
+    
     // Render icon based on the state of isPlaying
-    renderAction() {
-        if(!this.state.isPlaying) {
-            return <FontAwesomeIcon className="play-button" icon={faPlayCircle} onClick={this.changeMediaControls}/>
-        }
-        else {
+    renderAction = () => {
+        if(this.state.isPlaying) {
             return <FontAwesomeIcon className="pause-button" icon={faPauseCircle} onClick={this.changeMediaControls}/>
         }
+        else {
+            return <FontAwesomeIcon className="play-button" icon={faPlayCircle} onClick={this.changeMediaControls}/>
+        }
     }
 
-    // Change state
+    // Change state on click
     changeMediaControls = () => {
-        if(this.state.isPlaying) {
-            this.setState({
-                isPlaying: false,     
-            }) 
-        } else {
-            this.setState({
-                isPlaying: true,     
-            })
-        }
+        this.state.isPlaying
+        ? this.setState({ isPlaying: false, isPaused: true })
+        : this.setState({ isPlaying: true, isPaused: false })
+        
+        this.toggleAudio(this.audio)   
+    }
+
+    // Pause or play audio sample
+    toggleAudio = (song) => {
+        song !== null
+            ? this.state.isPlaying ? song.pause() : song.play() 
+            : console.log('no track') 
+    }
+
+    endOfSong = (song) => {
+        song.ended 
+        ? console.log('the song has ended') 
+        : console.log('the song is still playing')
+    }
+
+    likeSong = () => {
+        this.state.isLiked 
+        ? this.setState({ isLiked: false })
+        : this.setState({ isLiked: true })
     }
 
     render() {
@@ -42,7 +62,8 @@ class Tracks extends  Component {
                     <div className="track-details">
                         <div className="track-buttons">
                             {this.renderAction()}
-                            <FontAwesomeIcon className="like-button" icon={faHeart}/>
+                            <FontAwesomeIcon className={this.state.isLiked ? "like-button-clicked" :"like-button-unclicked"} 
+                            icon={faHeart} onClick={this.likeSong}/>
                         </div>
                         <p className="details">{track.artist} | {track.name}</p>
                         <p className="details">{track.album}</p>
